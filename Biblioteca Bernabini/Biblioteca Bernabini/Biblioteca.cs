f8ea8b6cac2d05976374fd7c2173ed90b432ec12
TheProgrammingSole <115690652+DieghitoRej@users.noreply.github.com>
2022-11-17 09:47:21 +0100

    Add files via upload

---
+++ Biblioteca Bernabini/Biblioteca Bernabini/Biblioteca.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Bernabini
+{
+    internal class Biblioteca
+    {
+        internal string Nome { get; set; }
+        internal string Indirizzo { get; set; }
+
+        internal double OrarioApertura { get; set; }
+        internal double OrarioChiusura { get; set; }
+        internal List<Libro> ListaLibri { get; set; }
+
+        internal void AggiungiLibro(string autore, string nome, string annoDiPublicazione, string editore, int numeroDiPagine)
+        {
+            Libro libroDaAggiungere = new Libro(autore, nome, annoDiPublicazione, editore, numeroDiPagine);
+            ListaLibri.Add(libroDaAggiungere);
+        }
+        internal void RicercaLibro()
+        {
+
+        }
+        internal void RicercaAutore()
+        {
+
+        }
+        internal int ContaLibri()
+        {
+            return ListaLibri.Count;
+        }
+
+
+    }
+}
---
+++ Biblioteca Bernabini/Biblioteca Bernabini/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Biblioteca_Bernabini
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// immagina dovere ripetere due commit perché lo screenshot è stato fatto alla consegna e non alla bash. bisogna veramente impegnarsi per sbagliare
+    /// a questi livelli!
+    public partial class MainWindow : Window
+    {
+        Biblioteca mondadori = new Biblioteca();
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+            
+        }
+
+        private void BtnAggiungiLibro_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRicercaAutore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnContaLibri_Click(object sender, RoutedEventArgs e)
+        {
+            BlkNumero.Text = mondadori.ContaLibri().ToString();
+        }
+
+        private void BtnRicercaLibro_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
